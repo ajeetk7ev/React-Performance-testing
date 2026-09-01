e51edd819923b53ca5d4cd51dfddb5c8de28ed34
ajeetk7ev <ajeetk8568@gmail.com>
2026-09-01 12:11:05 +0530

    Add the custom rolldown

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig({
   plugins: [
     react(),
+
     visualizer({
       filename: "dist/stats.html",
       open: true,
@@ -12,4 +13,56 @@ export default defineConfig({
       brotliSize: true,
     }),
   ],
+
+  build: {
+    rolldownOptions: {
+      output: {
+        // codeSplitting: {
+        //   groups: [
+        //     {
+        //       name: "vendor",
+        //       test: /node_modules/,
+        //     },
+        //   ],
+        // },
+
+        // codeSplitting: {
+        //   groups: [
+        //     {
+        //       name: "react-vendor",
+        //       test: /node_modules[\\/]react(?:-dom)?[\\/]/,
+        //       priority: 30,
+        //     },
+
+        //     {
+        //       name: "vendor",
+        //       test: /node_modules[\\/]/,
+        //       priority: 10,
+        //     },
+        //   ],
+        // },
+        codeSplitting: {
+  groups: [
+    {
+      name: "react-vendor",
+      test: /node_modules[\\/]react(?:-dom)?[\\/]/,
+      priority: 30,
+    },
+
+    {
+      name: "chart-vendor",
+      test: /node_modules[\\/](recharts|d3-[^\\/]+)[\\/]/,
+      priority: 20,
+    },
+
+    {
+      name: "vendor",
+      test: /node_modules[\\/]/,
+      priority: 10,
+    },
+  ],
+},
+      },
+    },
+  },
 });
